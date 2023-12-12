e73abda5082c5c3b0e25578415ce80ac1e04fa7c
Grigurg <grisha_8x@mail.ru>
2023-12-12 09:42:36 +0300

    `

--- core/src/com/none/story/GameScreen.java
+++ core/src/com/none/story/GameScreen.java
@@ -1,13 +1,15 @@
 package com.none.story;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameScreen extends ScreenAdapter {
     SpriteBatch batch;
     OrthographicCamera camera;
-    LoadScreen loadScreen;
+    InfoScreen loadScreen;
     CringeStory cringeStory;
 
     public static final int SCR_WIDTH = 1920;
@@ -15,13 +17,21 @@ public class GameScreen extends ScreenAdapter {
 
     GameScreen(CringeStory cringeStory) {
         this.cringeStory = cringeStory;
-        loadScreen = new LoadScreen(this);
+        String text = "Some cool text";
+        loadScreen = new InfoScreen(this, "main_background2.jpg", text);
         batch = new SpriteBatch();
         camera = new OrthographicCamera();
     }
     @Override
     public void show() {
         camera.setToOrtho(false, SCR_WIDTH, SCR_HEIGHT);
+//        batch.begin();
+//        font.draw(batch, "Hello", 50, 50);
+//        batch.end();
         cringeStory.setScreen(loadScreen);
+    }
+
+    public static BitmapFont getFont() {
+        return new BitmapFont(Gdx.files.internal("ch/font.fnt"), Gdx.files.internal("ch/font.png"), false);
     }
 }
